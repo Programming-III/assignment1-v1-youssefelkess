0202197d840890071182bb653e0eeca700f65c47
yousseftoma16009508 <youssefelkesg92021@gmail.com>
2025-11-12 15:08:24 +0200

    Add Mammal class definition with fur color
    Define the Mammal class inheriting from Animal with fur color attributes and methods.

--- Mammal.h
+++ Mammal.h
@@ -3,8 +3,25 @@
 
 #include "Animal.h"
 
-//define mammal class here
-
+class Mammal():public Animal(){
+      private:
+      string furColor;
+      public:
+      Mammal(){
+          furColor="";
+      }
+      Mammal(string c):Animal(string n,int a,bool hungry){
+          furColor=c;
+      }
+      ~Mammal(){}
+      void setFurColor(srting c){
+          furColor=c;
+      }
+      string getFurColor(){
+          return furColor;
+      }
+      
+  };
 
 
 
