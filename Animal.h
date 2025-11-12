f8517ff0985bc0ae837cf2bc50939954d2dbaa9b
yousseftoma16009508 <youssefelkesg92021@gmail.com>
2025-11-12 15:07:27 +0200

    Define Animal class with properties and methods
    Fix constructor initialization and correct feed method logic.

--- Animal.h
+++ Animal.h
@@ -4,7 +4,55 @@
 #include <string>
 using namespace std;
 
-//define animal class here 
+class Animal{
+  private:
+  string name;
+  int age;
+  bool isHungry;
+  public:
+  Animal(){
+  name="";
+  int=0;
+  bool=true;
+  }
+  Animal(string n,int a,bool hungry){
+      name=n;
+      age=a;
+      isHungry=hungry;
+  }
+  ~Animal(){}
+  void setName(string n){
+      name=n;
+  }
+  void setAge(int a){
+      age=a;
+  }
+  void setIsHungry(bool h){
+      isHungry=h;
+  }
+  string getName(){
+      return name;
+  }
+  int getAge(){
+      return age;
+  }
+  bool getIsHungry(){
+      return isHungry;
+  }
+  void display(){
+      cout<<"Name:"<<name<<endl;
+      cout<<"Age:"<<age<<endl;
+      cout<<"Hunger Status:"<<isHungry<<endl;
+  }
+  void feed(){
+      if(isHungry=true){
+          cout<<"hungry"<<endl;
+      }
+      else{
+          cout<<"not hungry"<<endl;
+      }
+  }
+};
 
 
 
