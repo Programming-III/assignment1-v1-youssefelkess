9cfb61422a91ceddfb8041db00a1c8eed80e7c68
yousseftoma16009508 <youssefelkesg92021@gmail.com>
2025-11-12 15:08:03 +0200

    Update Bird.h

--- Bird.h
+++ Bird.h
@@ -3,7 +3,25 @@
 
 #include "Animal.h"
 
-//define class bird here 
+class Mammal():public Animal(){
+      private:
+      string furColor;
+      public:
+      Mammal(){
+          furColor="";
+      }
+      Mammal(string c):Animal(string n,int a,bool hungry){
+          furColor=c;
+      }
+      ~Mammal(){}
+      void setFurColor(srting c){
+          furColor=c;
+      }
+      string getFurColor(){
+          return furColor;
+      }
+      
+  };
 
 
 
