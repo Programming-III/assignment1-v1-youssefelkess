249c972a712f023d306b88255d7973a76ce393b2
yousseftoma16009508 <youssefelkesg92021@gmail.com>
2025-11-12 15:10:51 +0200

    Add Enclosure class with constructors and methods
    Define the Enclosure class with constructors and methods.

--- Enclosure.h
+++ Enclosure.h
@@ -3,7 +3,31 @@
 
 #include "Animal.h"
 
-//define enclosure class here 
+class Enclosure{
+      private:
+      Animal* Animal;
+      int capacity;
+      int currentCount;
+      public:
+      Enclosure(){
+      Animal* =nullptr;
+      capacity=0;
+       currentCount=0;
+      }
+      Enclosure(Animal* a,int ca,int cur){
+        Animal*=a;  
+        capacity=ca;
+        currentCount=cur;
+      }
+      ~Enclosure(){}
+      delete[]Animal*;
+      void addAnimal(Animal*a){
+          
+      }
+      void displayAnimals(){
+          
+      }
+  };
 
 
 
