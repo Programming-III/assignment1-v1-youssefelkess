a1cbe7a0aca96bf1c72a5ee456e351cc4730689d
yousseftoma16009508 <youssefelkesg92021@gmail.com>
2025-11-12 15:19:16 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -10,7 +10,23 @@ using namespace std;
 
 // ============== MAIN FUNCTION ==============
 int main() {
+      
+   Enclosure enc();
+
+   
+   Animal animal1("Age:5",Hungry);
+    Animal animal2("Age:2",not Hungry);
+     Animal animal3("Age:3",Hungry);
+
     
+    Visitor .displayInfo();
+
+    
+    enclosure.addAnimal(a1);
+      enclosure.addAnimal(a2);
+        enclosure.addAnimal(a3);
+    animal.feed(a);
+
     
     return 0;
 }
