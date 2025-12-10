56b14cc6834dbca861aa124b0a5e3ec3961da71c
yousseftoma16009508 <youssefelkesg92021@gmail.com>
2025-12-10 14:01:41 +0200

    Update Enclosure.h

--- Enclosure.h
+++ Enclosure.h
@@ -27,6 +27,30 @@ class Enclosure{
       void displayAnimals(){
           
       }
+ void operator+=(Animal* animal) {
+        animals.push_back(animal);
+    }
+    
+    Animal* operator[](const string& name) {
+        for (Animal* animal : animals) {
+            if (animal->getName() == name) {
+                return animal;
+            }
+        }
+        return nullptr;
+    }
+    
+    friend ostream& operator<<(ostream& os, const Enclosure& e) {
+        os << "Enclosure contains " << e.animals.size() << " animals:\n";
+        for (size_t i = 0; i < e.animals.size(); i++) {
+            os << "  " << *(e.animals[i]) << "\n";
+        }
+        return os;
+    }
+    
+    const vector<Animal*>& getAnimals() const {
+        return animals;
+    }
   };
 
 
